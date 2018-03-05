84fba19d7d414c7a4adde4b456ca1c73a868bc38
eeapai <pa.eeapai@gmail.com>
2018-03-05 20:18:29 +0100

    Added STM32 USB comm lib

---
+++ Sources/Libs/Comm/WinUSB/WinUSBSTM32Device.cpp
@@ -0,0 +1,240 @@
+////////////////////////////////////////////////////////////////////////////////////
+// BSD 3-Clause License                                                           //
+//                                                                                //
+// Copyright (c) 2018, pa.eeapai@gmail.com                                        //
+// All rights reserved.                                                           //
+//                                                                                //
+// Redistribution and use in source and binary forms, with or without             //
+// modification, are permitted provided that the following conditions are met:    //
+//                                                                                //
+// * Redistributions of source code must retain the above copyright notice, this  //
+//   list of conditions and the following disclaimer.                             //
+//                                                                                //
+// * Redistributions in binary form must reproduce the above copyright notice,    //
+//   this list of conditions and the following disclaimer in the documentation    //
+//   and/or other materials provided with the distribution.                       //
+//                                                                                //
+// * Neither the name of the copyright holder nor the names of its                //
+//   contributors may be used to endorse or promote products derived from         //
+//   this software without specific prior written permission.                     //
+//                                                                                //
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"    //
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE      //
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE //
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE   //
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL     //
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR     //
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER     //
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,  //
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE  //
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.           //
+////////////////////////////////////////////////////////////////////////////////////
+
+#include "WinUSBSTM32Device.h"
+#include "WinUSBComm2Shared.h"
+#include <string.h>
+
+CWinUSBSTM32Device::CWinUSBSTM32Device(unsigned char *pbyBuffer, unsigned long dwBufferSizeInBytes) :
+  m_pbyBuffer(pbyBuffer),
+  m_dwBufferSizeInBytes(dwBufferSizeInBytes)
+{
+
+}
+
+void CWinUSBSTM32Device::Connect(const char * pcszWhereTo)
+{
+}
+
+int CWinUSBSTM32Device::GetStatus() const
+{
+  return ICommDevice::connectionConnected;
+}
+
+void CWinUSBSTM32Device::Send(const unsigned char * pbyData, unsigned long dwByteCount, unsigned long * pdwSentByteCount)
+{
+  if ( m_dwSendByteCountUSB )
+  {
+    if ( pdwSentByteCount )
+    {
+      *pdwSentByteCount = 0;
+    }
+  }
+  else
+  {
+    memmove(m_pbyBuffer, pbyData, dwByteCount);
+    if ( pdwSentByteCount )
+    {
+      *pdwSentByteCount = dwByteCount;
+    }
+    m_dwSendByteCountUSB = dwByteCount;
+  }
+}
+
+void CWinUSBSTM32Device::Receive(unsigned char * pbyDestination, unsigned long dwMaxByteCount, unsigned long * pdwHowManyBytes)
+{
+  if ( m_dwReceivedByteCount )
+  {
+    memmove(pbyDestination, m_pbyBuffer, m_dwReceivedByteCount);
+    if ( pdwHowManyBytes )
+    {
+      *pdwHowManyBytes = m_dwReceivedByteCount;
+    }
+    m_dwReceivedByteCount = 0;
+  }
+  else
+  {
+    if ( pdwHowManyBytes )
+    {
+      *pdwHowManyBytes = 0;
+    }
+  }
+}
+
+void CWinUSBSTM32Device::Disconnect()
+{
+}
+
+static unsigned char s_byWinUSBCommVersion = winusbcommversion2;
+
+SWinUSBSetupResponse CWinUSBSTM32Device::SetupVendorInterface(unsigned char byRequest)
+{
+  SWinUSBSetupResponse response = { 0 };
+  response.m_eResponse = ewinusbsetupresponseSendToHostOnEP0; // by defualt
+  switch ( byRequest )
+  {
+  case winusbcomm2commandReset:               response.m_eResponse = ewinusbsetupresponseOK; m_dwExpectedByteCountUSB = 0; m_dwSendByteCountUSB = 0; break;
+  case winusbcomm2commandGetVersion:          response.m_pbyData = &s_byWinUSBCommVersion; response.m_dwByteCount = 1; break;
+  case winusbcomm2commandGetState:            response.m_pbyData = &m_byStateUSB; response.m_dwByteCount = 1; break;
+  case winusbcomm2commandGetBufferSize:       response.m_pbyData = (unsigned char *)(&m_dwBufferSizeInBytes); response.m_dwByteCount = 4; break;
+  case winusbcomm2commandGetReturnSize:       response.m_pbyData = (unsigned char *)(&m_dwSendByteCountUSB); response.m_dwByteCount = 4; break;
+  case winusbcomm2commandFollowingPacketSize: response.m_eResponse = ewinusbsetupresponseReceiveFromHostOnEP0; m_dwReceivedByteCount = 0; response.m_pbyData = (unsigned char *)(&m_dwExpectedByteCountUSB); response.m_dwByteCount = 4; break;
+  default:                                    response.m_eResponse = ewinusbsetupresponseError; break;
+  }
+  return response;
+}
+
+void CWinUSBSTM32Device::DataIn()
+{
+  m_dwSendByteCountUSB = 0; // is this OK?
+  updateState();
+}
+
+SWinUSBSetupResponse CWinUSBSTM32Device::EP0_RxReady(unsigned char byRequest)
+{
+  SWinUSBSetupResponse response = { 0 };
+  response.m_eResponse = ewinusbsetupresponseOK;
+  if ( winusbcomm2commandFollowingPacketSize == byRequest )
+  {
+    response.m_eResponse = ewinusbsetupresponseReceiveFromHostOnEPy;
+    response.m_pbyData = m_pbyBuffer;
+    response.m_dwByteCount = m_dwExpectedByteCountUSB;
+  }
+  updateState();
+  return response;
+}
+
+SWinUSBSetupResponse CWinUSBSTM32Device::EP0_TxReady(unsigned char byRequest)
+{
+  SWinUSBSetupResponse response = { 0 };
+  response.m_eResponse = ewinusbsetupresponseOK;
+  if ( ( winusbcomm2commandGetReturnSize == byRequest ) && m_dwSendByteCountUSB )
+  {
+    response.m_eResponse = ewinusbsetupresponseSendToHostOnEPx;
+    response.m_pbyData = m_pbyBuffer;
+    response.m_dwByteCount = m_dwSendByteCountUSB;
+  }
+  updateState();
+  return response;
+}
+
+void CWinUSBSTM32Device::DataOut()
+{
+  // On STM32F103 this is called after complete transfer data arrived with multiple EP sized packets.
+  // USBD_LL_GetRxDataSize returns size of last packet.
+  // uint32_t dwNumBytesReceived = USBD_LL_GetRxDataSize(pdev, psWinUSBCommSTM32->m_byEPOutAddress);
+  m_dwReceivedByteCount += m_dwExpectedByteCountUSB;
+  updateState();
+}
+
+
+void CWinUSBSTM32Device::updateState()
+{
+  unsigned long dwReceivedByteCountUSB = m_dwReceivedByteCount;
+
+  // If expecting new data
+  if ( m_dwExpectedByteCountUSB )
+  {
+    // It's probably receiving state
+    if ( m_dwSendByteCountUSB )
+    {
+      // But not if data also scheduled to send - error
+      m_byStateUSB = winusbcomm2stateError;
+      return;
+    }
+    if ( dwReceivedByteCountUSB < m_dwExpectedByteCountUSB )
+    {
+      // Still not everything received - receiving
+      m_byStateUSB = winusbcomm2stateReceiving;
+      return;
+    }
+    // Everything received - idle
+    m_byStateUSB = winusbcomm2stateIdle;
+    return;
+  }
+
+  // Not expecting any more data
+  if ( dwReceivedByteCountUSB )
+  {
+    // Data remains received
+    if ( m_dwSendByteCountUSB )
+    {
+      // If data scheduled to send - error
+      m_byStateUSB = winusbcomm2stateError;
+      return;
+    }
+    // idle
+    m_byStateUSB = winusbcomm2stateIdle;
+    return;
+  }
+
+  // Nothing to receive and nothing received
+  if ( m_dwSendByteCountUSB )
+  {
+    // Data scheduled to send - sending
+    m_byStateUSB = winusbcomm2stateSending;
+    return;
+  }
+
+  // Nothing to receive, nothing received, and nothing to send - idle
+  m_byStateUSB = winusbcomm2stateIdle;
+}
+
+SWinUSBSetupResponse WinUSBComm_SetupVendorInterface(void *psWinUSBCommSTM32, unsigned char byRequest)
+{
+  CWinUSBSTM32Device *pCommDev = (CWinUSBSTM32Device *)psWinUSBCommSTM32;
+  return pCommDev->SetupVendorInterface(byRequest);
+}
+
+void WinUSBComm_DataIn(void *psWinUSBCommSTM32)
+{
+  CWinUSBSTM32Device *pCommDev = (CWinUSBSTM32Device *)psWinUSBCommSTM32;
+  pCommDev->DataIn();
+}
+
+SWinUSBSetupResponse WinUSBComm_EP0_RxReady(void *psWinUSBCommSTM32, unsigned char byRequest)
+{
+  CWinUSBSTM32Device *pCommDev = (CWinUSBSTM32Device *)psWinUSBCommSTM32;
+  return pCommDev->EP0_RxReady(byRequest);
+}
+
+SWinUSBSetupResponse WinUSBComm_EP0_TxReady(void *psWinUSBCommSTM32, unsigned char byRequest)
+{
+  CWinUSBSTM32Device *pCommDev = (CWinUSBSTM32Device *)psWinUSBCommSTM32;
+  return pCommDev->EP0_TxReady(byRequest);
+}
+
+void WinUSBComm_DataOut(void *psWinUSBCommSTM32)
+{
+  CWinUSBSTM32Device *pCommDev = (CWinUSBSTM32Device *)psWinUSBCommSTM32;
+  pCommDev->DataOut();
+}
---
+++ Sources/Libs/Comm/WinUSB/WinUSBSTM32Device.h
@@ -0,0 +1,102 @@
+////////////////////////////////////////////////////////////////////////////////////
+// BSD 3-Clause License                                                           //
+//                                                                                //
+// Copyright (c) 2018, pa.eeapai@gmail.com                                        //
+// All rights reserved.                                                           //
+//                                                                                //
+// Redistribution and use in source and binary forms, with or without             //
+// modification, are permitted provided that the following conditions are met:    //
+//                                                                                //
+// * Redistributions of source code must retain the above copyright notice, this  //
+//   list of conditions and the following disclaimer.                             //
+//                                                                                //
+// * Redistributions in binary form must reproduce the above copyright notice,    //
+//   this list of conditions and the following disclaimer in the documentation    //
+//   and/or other materials provided with the distribution.                       //
+//                                                                                //
+// * Neither the name of the copyright holder nor the names of its                //
+//   contributors may be used to endorse or promote products derived from         //
+//   this software without specific prior written permission.                     //
+//                                                                                //
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"    //
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE      //
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE //
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE   //
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL     //
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR     //
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER     //
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,  //
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE  //
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.           //
+////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WINUSB_STM32_DEVICE_H__
+#define __WINUSB_STM32_DEVICE_H__
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+typedef enum _EWinUSBSetupResponse
+{
+  ewinusbsetupresponseOK,
+  ewinusbsetupresponseError,
+  ewinusbsetupresponseSendToHostOnEP0,
+  ewinusbsetupresponseReceiveFromHostOnEP0,
+  ewinusbsetupresponseSendToHostOnEPx,
+  ewinusbsetupresponseReceiveFromHostOnEPy,
+}EWinUSBSetupResponse;
+typedef struct _SWinUSBSetupResponse
+{
+  unsigned long m_dwByteCount;
+  unsigned char *m_pbyData;
+  EWinUSBSetupResponse m_eResponse;
+}SWinUSBSetupResponse;
+SWinUSBSetupResponse WinUSBComm_SetupVendorInterface(void *psWinUSBCommSTM32, unsigned char byRequest);
+void WinUSBComm_DataIn(void *psWinUSBCommSTM32);
+SWinUSBSetupResponse WinUSBComm_EP0_RxReady(void *psWinUSBCommSTM32, unsigned char byRequest);
+SWinUSBSetupResponse WinUSBComm_EP0_TxReady(void *psWinUSBCommSTM32, unsigned char byRequest);
+void WinUSBComm_DataOut(void *psWinUSBCommSTM32);
+
+#ifdef __cplusplus
+}
+
+#include "ICommDevice.h"
+
+///< Type for WinUSB interface data transfer context on STM32
+class CWinUSBSTM32Device : public ICommDevice
+{
+  public:
+    CWinUSBSTM32Device(unsigned char *pbyBuffer, unsigned long dwBufferSizeInBytes);
+  private:
+  unsigned char *m_pbyBuffer;               ///< Pointer to comm buffer
+  unsigned long m_dwBufferSizeInBytes;      ///< Size of comm buffer
+
+  unsigned long m_dwExpectedByteCountUSB = 0;   ///< Current incoming packet length
+
+  unsigned long m_dwSendByteCountUSB = 0;       ///< Size of packet to send
+  unsigned long m_dwReceivedByteCount = 0;      ///< Size of received packet
+
+  unsigned char m_byStateUSB = 0;               ///< Of EWinUSBComm2State
+
+public:
+  void Connect(const char * pcszWhereTo) override;
+  int GetStatus() const override;
+  void Send(const unsigned char * pbyData, unsigned long dwByteCount, unsigned long * pdwSentByteCount) override;
+  void Receive(unsigned char * pbyDestination, unsigned long dwMaxByteCount, unsigned long * pdwHowManyBytes) override;
+  void Disconnect() override;
+
+public:
+  SWinUSBSetupResponse SetupVendorInterface(unsigned char byRequest);
+  void DataIn();
+  SWinUSBSetupResponse EP0_RxReady(unsigned char byRequest);
+  SWinUSBSetupResponse EP0_TxReady(unsigned char byRequest);
+  void DataOut();
+private:
+  void updateState();
+};
+
+#endif
+
+
+#endif
